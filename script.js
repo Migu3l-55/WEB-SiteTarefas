b70feb0643a6818ec3e44264dce06f545b6c9db5
Migu3l-55 <mig355mendonca@gmail.com>
2025-02-26 16:23:35 -0300

    exibir tabela de listade tarefas

--- script.js
+++ script.js
@@ -8,11 +8,35 @@ function tarefa(tTitulo, tData, tPrioridade) {
     this.tPrioridade = tPrioridade; 
 }
 
+
+// quando coloca o titulo e a data, o array reinicia...
 botao.addEventListener("click", function(){ 
     let titulo = document.querySelector("#tarefa").value;
     let data = document.querySelector("#dataVencimento").value;
     let prioridade = document.querySelector("#prioridade").value;
 
     arrayDeTarefas.push(new tarefa(titulo, data, prioridade));
+    exibirListaTarefas()
 });
 
+// tá funcionando, mas quando coloca o titulo e a data, o array reinicia...
+function exibirListaTarefas() {
+    alert(arrayDeTarefas.length);
+    let elemento = document.querySelector("#tbody-tarefas");
+    let text = ""
+    text = `<tr>
+                <th>tarefa</th>
+                <th>data</th>
+                <th>prioridade</th>
+            </tr>`;
+    for (let x = 0; x < arrayDeTarefas.length; x++) {
+        text += `
+        <tr>
+            <td>${arrayDeTarefas[x].tTitulo}</td>
+            <td>${arrayDeTarefas[x].tData}</td>
+            <td>${arrayDeTarefas[x].tPrioridade}</td>
+        </tr>`;
+    }
+    elemento.innerHTML = text;
+    alert(arrayDeTarefas.length);
+}
